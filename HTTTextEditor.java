b6003e302a03939ddfa5bdc1f54e7809b0a69c4e
Harley <51787264+Harleythetech@users.noreply.github.com>
2022-05-26 23:33:54 +0800

    Add files via upload

---
+++ HTTTextEditor.java
@@ -0,0 +1,20 @@
+package com.harleythetech.sbphmrup.htt.texteditor;
+
+
+public class HTTTextEditor extends javax.swing.JFrame {
+
+    public static void main(String args[]) {
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new Jframe().setVisible(true);
+        });
+    }
+        
+}
+/**
+ * @author @Harleythetech - Vincent Harley R. Jovellano - ICT102A - [28d54af0-e2e2-4dd2-92db-234854c0119f]
+ * @MD5: 384381ae033564c2e5d6736814bac802
+ * @SHA1: c39e45c367e4cdbfd6b5e910b03efe1f9bdcd355
+ * @SHA256: 91c59d44fa3a8828b7b55924f2006d6e847bc769c9da0334976b93b53db6ccab
+ * @SHA512: 47e430b9ad9c9ddd7f5559d66770481e33360e08e82bba131203a01ea6a492413b08bd32fb05f31cde67ce72391620f4fe3ffbeb83cde0a7ba05a8f072423d92
+ */
